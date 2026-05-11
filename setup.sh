3ad8061487476e683ae1c5e98ae073159902700f
khainp <38685140+khaikenly@users.noreply.github.com>
2026-05-11 14:32:30 +0700

    Create setup.sh for repo updates and Docker setup
    Script to update repositories and manage Docker containers.

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+declare branch='main'
+
+for dir in ./*                                                
+do                                                                                 
+    cd $dir
+    git pull origin $branch
+    cp .env.example .env
+    cd ..
+fi                                                                
+done
+
+docker compose -f ./cmp-base.yml \\
+        ./arxiv-crawler/docker-compose.yml \\
+        ./backend/docker-compose.yml \\
+        ./paper-system-fe/docker-compose.yml down
+
+docker compose -f ./cmp-base.yml \\
+        ./arxiv-crawler/docker-compose.yml \\
+        ./backend/docker-compose.yml \\
+        ./paper-system-fe/docker-compose.yml up -d --build
